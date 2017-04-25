6f2e54a46dd0f721615cf3a696809aca46fffa96
wallism <mark@failfast.net>
2017-04-26 00:18:14 +0100

    template deploy working - first cut

---
+++ consul/azure/ExportedTemplate-consul-rg-one/run-consul-deploy-ff.ps1
@@ -0,0 +1,6 @@
+& ./deploy.ps1 -subscriptionId "53ccb882-db18-41e1-8b79-5babec22be1b" `
+ -resourceGroupName "consul-rg-1" `
+ -resourceGroupLocation "Australia East" `
+ -deploymentName "deployment-name-20170426-0830" `
+ -templateFilePath "template.json" `
+ -parametersFilePath "template-params-consul.json"
